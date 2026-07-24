da589e678036508d6f63b2de6ce5c04276afd20a
Leon Ribas <leon.pessoa@gmail.com>
2026-07-24 02:00:53 -0300

    Add Main class for Fibonacci sequence calculation

---
+++ src/exercises/beginner/p1176/Main.java
@@ -0,0 +1,27 @@
+package exercises.beginner.p1176;
+
+import java.util.Scanner;
+
+public class Main {
+    public static long fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        long[] fib = new long[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[n];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            System.out.println("Fib(" + N + ") = " + fib(N));
+        }
+    }
+}
